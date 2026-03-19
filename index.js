df6891535524a75df55e00b86ddda23b530bc937
Arsh <arshkhancloud@gmail.com>
2026-03-19 09:30:29 +0530

    added web server

--- index.js
+++ index.js
@@ -266,3 +266,14 @@ setInterval(async () => {
         }
     }
 }, 15000);
+
+//SETUP UPTIMEROBOT
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Bot is running 🚀");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Web server running..."));
